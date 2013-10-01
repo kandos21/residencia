15db79054213060f070286c49f4030e6f44a8ab5
kandos21 <kandos21@gmail.com>
2013-10-01 13:27:41 -0500

    corregido envio general
    ya se guarda fecha y envio. terminado

--- subir.php
+++ subir.php
@@ -7,6 +7,11 @@
 ?>
 
 <?php 
+          $envio=$_POST["envio"];
+          $fecha=$_POST["fecha"];
+
+
+
                       if(isset($_POST['radio']))
                       {
                          //subir la imagen del articulo
@@ -78,15 +83,24 @@
                     $curp_ben=$objCelda['M'];
                     $curp_rep=$objCelda['N'];
 
+                    if (persona==$persona_rep) 
+                    {
+                      $categoria=1;
+                    }
+                    else
+                    {
+                      $categoria=2;
+                    }
+
                            
                   
         if($_POST['radio']=='s'){
             $sql_general="INSERT INTO general
-            (g_expediente, g_nombre_region, g_region, g_municipio, g_localidad, g_organizacion, g_elector_rep, g_nombre_rep, g_elector_ben, g_nombre_ben, g_monto_sol, g_cantidad, g_curp_ben, g_curp_rep)
-            VALUES ('$n_expediente2','$nombre_region','$region','$municipio2','$localidad2','$organizacion2','$elector_rep','$nombre_rep','$elector_ben','$nombre_ben','$monto_sol','$cantidad','$curp_ben','$curp_rep')";
+            (g_expediente, g_region, g_nombre_region, g_municipio, g_localidad, g_organizacion, g_elector_rep, g_nombre_rep, g_elector_ben, g_nombre_ben, g_monto_sol, g_cantidad, g_curp_ben, g_curp_rep, g_envio, g_fecha_r)
+            VALUES ('$n_expediente2','$region','$nombre_region','$municipio2','$localidad2','$organizacion2','$elector_rep','$nombre_rep','$elector_ben','$nombre_ben','$monto_sol','$cantidad','$curp_ben','$curp_rep','$envio','$fecha')";
             
 
-             $sql_municipio="INSERT INTO municipio
+             /*$sql_municipio="INSERT INTO municipio
             ( nombre_municipio)
             VALUES ('$municipio2')";
 
@@ -99,16 +113,16 @@
             VALUES ('$organizacion2')";
 
             $sql_representante="INSERT INTO persona(nombre_persona, elector, curp, categoria) 
-            VALUES('$nombre_rep','$elector_rep','$curp_rep')";
+            VALUES('$nombre_rep','$elector_rep','$curp_rep','$categoria','id_municipio')";
 
             $sql_beneficiario="INSERT INTO persona(nombre_persona,elector,curp,categoria)
-            VALUES('$elector_ben','$nombre_ben','$curp_ben' )";
+            VALUES('$elector_ben','$nombre_ben','$curp_ben','$categoria' )";
 
             $sql_monto="INSERT INTO monto(monto_sol)
             VALUES('$monto_sol')";
              
            $sql_expediente="INSERT INTO expediente(exp, envio, fecha_r,)
-           VALUES('$n_expediente2','$envio','$fecha')";
+           VALUES('$n_expediente2','$envio','$fecha')";*/
 
             
             
@@ -116,13 +130,13 @@
 
 
             mysql_query($sql_general);
-            mysql_query($sql_municipio);
+           /* mysql_query($sql_municipio);
             mysql_query($sql_localidad);
             mysql_query($sql_organizacion);
             mysql_query($sql_representante);
             mysql_query($sql_beneficiario);
             mysql_query($sql_monto);
-            mysql_query($sql_expediente);
+            mysql_query($sql_expediente);*/
 
            
 
